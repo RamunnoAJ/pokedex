860066ace4c23ec9bbe2a590d37aae5c743c9b6f
RamunnoAJ <ramunnoagustin@gmail.com>
2023-03-31 16:32:52 -0300

    move: fetch function to utils

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -20,3 +20,9 @@ export function roundStringToThreeChars(string) {
 
   return string
 }
+
+export async function fetchURL(url) {
+  const response = await fetch(url)
+
+  return await response.json()
+}
