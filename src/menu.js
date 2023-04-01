312fe4830b8672f21ae6a671d1118a1fea96bd86
RamunnoAJ <ramunnoagustin@gmail.com>
2023-04-01 14:32:08 -0300

    move file to ui folder

--- src/menu.js
+++
@@ -1,8 +0,0 @@
-const $btnMenu = document.querySelector('#btn-menu')
-const $navLinks = document.querySelector('#nav-links')
-const $nav = document.querySelector('.nav')
-
-$btnMenu.addEventListener('click', () => {
-  $nav.classList.toggle('menu-open')
-  $navLinks.classList.toggle('active')
-})
