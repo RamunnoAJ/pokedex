5dbd852bbb17b40c1457d40114a9472102904650
RamunnoAJ <ramunnoagustin@gmail.com>
2023-04-03 13:01:40 -0300

    add: function to get pokemon list from Local Storage

--- src/storage/pokemon.js
+++ src/storage/pokemon.js
@@ -1,14 +1,31 @@
+function getPokemonListKey(offset, limit) {
+  return `pokemon_${offset}_${limit}`
+}
+
 function getPokemonKey(id) {
   return `pokemon_${id}`
 }
 
 export function getPokemon(id) {
   if (id === undefined)
-    throw new Error('You should pass an id to load the pokemon')
+    throw new Error('You should pass an id to get the pokemon')
 
   const pokemon = JSON.parse(localStorage.getItem(getPokemonKey(id)))
 
   if (pokemon === null) throw new Error(`Pokemon with id: ${id} not founded`)
 
   return pokemon
+}
+
+export function getPokemonList(offset = 0, limit = 20) {
+  const pokemonList = JSON.parse(
+    localStorage.getItem(getPokemonListKey(offset, limit))
+  )
+
+  if (pokemonList === null)
+    throw new Error(
+      `Pokemon list with offset ${offset} and limit ${limit} not founded`
+    )
+
+  return pokemonList
 }
