0a042dfd475a8be3ff477ff6f3880b1df6f86913
RamunnoAJ <ramunnoagustin@gmail.com>
2023-07-15 13:34:07 -0300

    add: unit testing for utils/functions.js

--- src/utils/__tests__/functions.spec.js
+++ src/utils/__tests__/functions.spec.js
@@ -1,44 +1,78 @@
-import { capitalizeFirstLetter, convertDecimetersToMeters, convertHectogramsToPounds, fetchURL, roundStringToThreeChars } from "../functions.js";
+import {
+  capitalizeFirstLetter,
+  convertDecimetersToMeters,
+  convertHectogramsToPounds,
+  fetchURL,
+  roundStringToThreeChars,
+} from '../functions.js'
 
-test("should capitalize the first letter of a string given a string", () => {
-  expect(capitalizeFirstLetter("hello")).toBe("Hello")
-})
+describe('capitalizeFirstLetter function', () => {
+  test('should capitalize the first letter of a string given a string', () => {
+    expect(capitalizeFirstLetter('hello')).toBe('Hello')
+  })
 
-test("should return null if the input is not a string", () => {
-  expect(capitalizeFirstLetter(1)).toBe(null)
-})
-
-test("should convert hectograms to pounds", () => {
-  expect(convertHectogramsToPounds(1)).toBe(0.220462)
-})
-
-test("should return null if the input is not a number", () => {
-  expect(convertHectogramsToPounds("hello")).toBe(null)
-})
-
-test("should convert decimeters to meters", () => {
-  expect(convertDecimetersToMeters(1)).toBe(0.1)
-})
-
-test("should return null if the input is not a number", () => {
-  expect(convertDecimetersToMeters("hello")).toBe(null)
-})
-
-test("should round a string to three characters", () => {
-  expect(roundStringToThreeChars("hello")).toBe("hel")    
-})
-
-test("should return null if the input is not a string", () => {
-  expect(roundStringToThreeChars(1)).toBe(null)
-})
-
-test("should fetch the url passed", () => { 
-  expect(fetchURL("https://pokeapi.co/api/v2/pokemon/1/")).resolves.toEqual({
-    name: "bulbasaur",
-    url: "https://pokeapi.co/api/v2/pokemon/1/"
+  test('should return null if the input is not a string', () => {
+    expect(capitalizeFirstLetter(1)).toBe(null)
   })
 })
 
-test("should throw an error if the input is not a string", () => {
-  expect(fetchURL(1)).rejects.toThrow()
+describe('convertHectogramsToPounds function', () => {
+  test('should convert hectograms to pounds', () => {
+    expect(convertHectogramsToPounds(1)).toBe(0.220462)
+  })
+
+  test('should return null if the input is not a number', () => {
+    expect(convertHectogramsToPounds('hello')).toBe(null)
+  })
+})
+
+describe('convertDecimetersToMeters function', () => {
+  test('should convert decimeters to meters', () => {
+    expect(convertDecimetersToMeters(1)).toBe(0.1)
+  })
+
+  test('should return null if the input is not a number', () => {
+    expect(convertDecimetersToMeters('hello')).toBe(null)
+  })
+})
+
+describe('roundStringToThreeChars function', () => {
+  test('should round a string to three characters', () => {
+    expect(roundStringToThreeChars('he')).toBe('0he')
+  })
+
+  test('should return null if the input is not a string', () => {
+    expect(roundStringToThreeChars(1)).toBe(null)
+  })
+})
+
+describe('fetchURL function', () => {
+  beforeEach(() => {
+    global.fetch = jest.fn()
+  })
+
+  test('should fetch the url passed', () => {
+    global.fetch.mockImplementationOnce(
+      () =>
+        new Promise(resolve => {
+          const jsonPromise = new Promise(res => {
+            res({})
+          })
+          resolve({ json: () => jsonPromise })
+        })
+    )
+
+    fetchURL('https://pokeapi.co/api/v2/pokemon/1')
+    expect(global.fetch).toHaveBeenCalledTimes(1)
+
+    expect(global.fetch).toHaveBeenCalledWith(
+      'https://pokeapi.co/api/v2/pokemon/1'
+    )
+  })
+
+  test('should throw an error if the input is not a string', () => {
+    expect(fetchURL()).rejects.toEqual(new Error('url must be a string'))
+
+    expect(global.fetch).toHaveBeenCalledTimes(0)
+  })
 })
