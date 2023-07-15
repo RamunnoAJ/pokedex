ec6f146bf3662ffee5f6fc779c15ac2c6986992f
RamunnoAJ <ramunnoagustin@gmail.com>
2023-07-15 13:33:40 -0300

    fix: setup for unit testing

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,7 @@
-const { defineConfig } = require('cypress')
+import { defineConfig } from 'cypress'
 
-module.exports = defineConfig({
+export default defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
     testIsolation: false,
     viewportHeight: 900,
     viewportWidth: 1280,
--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,13 @@
-module.exports = {
+export default {
   verbose: true,
-  rootDir: 'src',
+  rootDir: './src',
   coverageDirectory: '../coverage/',
-  testPathIgnorePatterns: ['/node_modules/', '.*fixture.js'],
-  coveragePathIgnorePatterns: ['/node_modules/', '.*fixture.js'],
-};
+  testPathIgnorePatterns: ['./node_modules/', '.*fixture.js'],
+  coveragePathIgnorePatterns: ['./node_modules/', '.*fixture.js'],
+  resetMocks: false,
+  setupFiles: ['jest-localstorage-mock'],
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.[t|j]sx?$': 'babel-jest',
+  },
+}
